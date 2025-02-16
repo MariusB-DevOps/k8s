15e65a884f56e5884d586f011fda8bfe565b39c1
Marius <marius@osips.eu>
2025-02-16 18:08:03 +0200

    Adjust EKS module outputs and remove undeclared variables

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -6,6 +6,10 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
+#output "cluster_certificate_authority_data" {
+#  value = aws_eks_cluster.your_cluster_name.certificate_authority[0].data
+#}
+
 output "cluster_certificate_authority_data" {
-  value = aws_eks_cluster.your_cluster_name.certificate_authority[0].data
+  value = module.eks_module.cluster_certificate_authority_data # Assuming the module has an output for this
 }
--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -2,6 +2,7 @@ resource "aws_route53_record" "jenkins" {
   zone_id = var.route53_zone_id  # Replace with your Route 53 hosted zone ID
   name    = var.jenkins_ingress_host
   type    = "A"
+  depends_on = [ingress_controller_public_dns]
 
   alias {
     name                   = var.ingress_controller_public_dns  # Replace with the public DNS of your ingress controller

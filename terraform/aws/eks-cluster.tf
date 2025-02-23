f120e12adf7cac089b339793519b5ed66b96fb86
Marius <marius@osips.eu>
2025-02-23 11:45:34 +0200

    Modified providers versions to latest

--- terraform/aws/eks-cluster.tf
+++ terraform/aws/eks-cluster.tf
@@ -1,16 +1,16 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.13.1"
+  version = "20.31"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.24"
+  cluster_version = "1.31"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type = "AL2_x86_64"   #Amazon ECS-Optimized Amazon Linux 2
 
   }
 
--- terraform/aws/terraform.tf
+++ terraform/aws/terraform.tf
@@ -7,22 +7,22 @@ terraform {
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4.3"
+      version = "~> 3.6.3"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.6"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = "~> 2.3.5"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.16.1"
+      version = "~> 2.35.1"
     }
   }
 

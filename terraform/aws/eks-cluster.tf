6e09bb5f9db65d3f3a23bded9f8cf27a4e80faef
Marius <marius@osips.eu>
2025-02-23 16:39:51 +0200

    added aws-auth module and config #4

--- terraform/aws/eks-cluster.tf
+++ terraform/aws/eks-cluster.tf
@@ -1,14 +1,3 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.33.1"
@@ -47,62 +36,29 @@ module "eks" {
   }
 }
 
-# Configure aws-auth ConfigMap
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+module "eks_aws-auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "20.33.1"
 
-  data = {
-    mapRoles = <<YAML
-- rolearn: ${aws_iam_role.eks_deployer.arn}
-  username: eks-deployer
-  groups:
-    - system:masters
-- rolearn: ${var.iam_role_arn}
-  username: eks-deployer
-  groups:
-    - system:masters
-YAML
+  manage_aws_auth_configmap = true
 
-    mapUsers = <<YAML
-- userarn: ${var.iam_user_arn}
-  username: ${var.iam_username}
-  groups:
-    - system:masters
-YAML
-  }
+  aws_auth_roles = [
+    {
+      rolearn  = var.iam_role_arn
+      username = "Eks"
+      groups   = ["system:masters"]
+    },
+  ]
 
-  depends_on = [module.eks]
-}
+  aws_auth_users = [
+    {
+      userarn  = var.iam_user_arn
+      username = var.iam_username
+      groups   = ["system:masters"]
+    },
+  ]
 
-# IAM Role for EKS Deployer
-resource "aws_iam_role" "eks_deployer" {
-  name = "eks-deployer"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          AWS = var.assume_role_principal_arn # Allow your root account to assume this role.
-        }
-      },
-    ]
-  })
-}
-
-# Attach AdministratorAccess Policy to the Role
-resource "aws_iam_role_policy_attachment" "eks_deployer_policy" {
-  role       = aws_iam_role.eks_deployer.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # or any other policy you want to attach.
+  aws_auth_accounts = [
+    "597088032758",
+  ]
 }

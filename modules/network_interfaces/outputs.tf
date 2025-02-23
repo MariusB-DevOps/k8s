b0c5e795a2d2840de141fd38473eb8d43597e6f7
Marius <marius@osips.eu>
2025-02-23 09:15:32 +0200

    Created empty branch

--- modules/network_interfaces/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_nic_ids" {
-  description = "The IDs of the public network interfaces"
-  value       = [for nic in aws_network_interface.public : nic.id]
-}
-
-output "private_nic_ids" {
-  description = "The IDs of the private network interfaces"
-  value       = [for nic in aws_network_interface.private : nic.id]
-}
--- modules/vpc/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "k8s_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_hostnames = var.enable_dns_hostnames
-  tags = {
-        Name        = "k8s_vpc"
-        Environment = "k8s"
-        Terraform   = "true"
-    }
-}

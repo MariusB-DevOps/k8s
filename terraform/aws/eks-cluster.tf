ee9f5fbfc604e2d5711899cc141bc69d65cb1d7a
Marius <marius@osips.eu>
2025-02-23 15:57:12 +0200

    added block to fix auth to eks cluster #4

--- terraform/aws/eks-cluster.tf
+++ terraform/aws/eks-cluster.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.33.1"

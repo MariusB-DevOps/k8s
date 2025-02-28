07c7001580c7dc5f20e557d4896a2d52c93a5a0e
Marius <marius@osips.eu>
2025-02-28 11:23:03 +0200

    rebased tf structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
   region = "eu-west-1"
 }
-# terraform {
-#   required_version = "1.5.7"
-#   backend "s3" {
-#     bucket = "your-bucket-name"
-#     key    = "your-bucket-key"
-#     region = "eu-west-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "mariusb-tf-state"
+    key    = "terraform/state/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
 
 locals {
   policies = ["arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy", "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"]

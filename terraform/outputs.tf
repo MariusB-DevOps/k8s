edde85119ae3db3ba8dfa9401ba2a209a6991661
Marius <marius@osips.eu>
2025-03-01 22:43:55 +0200

    the real test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,18 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.main.name
 }
 
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.main.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "eks_cluster_certificate_authority" {
+  description = "The certificate authority for EKS"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+

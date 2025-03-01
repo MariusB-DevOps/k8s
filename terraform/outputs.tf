39fd5a297df6a8f4a0e3fa1494862a68002d4e77
Marius <marius@osips.eu>
2025-03-01 22:23:09 +0200

    the real test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,11 +6,3 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.main.name
 }
 
-output "argocd_lb_dns_name" {
-  value = aws_lb.argocd_lb.dns_name
-}
-
-output "argocd_lb_zone_id" {
-  value = aws_lb.argocd_lb.zone_id
-}
-

2e55eefbe8079dd354efe201d9904de6c2b7fca2
Marius <marius@osips.eu>
2025-03-01 13:08:53 +0200

    moved jenkins alb config to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,20 +167,20 @@ resource "aws_lb_listener" "jenkins_https" {
   load_balancer_arn = aws_lb.jenkins_alb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy       = "ELBSecurityPolicy-2016-08"
-  certificate_arn  = aws_acm_certificate.jenkins_cert.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.jenkins_cert.arn
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.jenkins_tg.arn
   }
 }
 
 resource "aws_lb_target_group" "jenkins_tg" {
-  name     = "jenkins-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "jenkins-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
   health_check {
